19fc46bb026ab9b932b0e6772bc32d10342c6128
madhavparekh <madhavparekh@gmail.com>
2018-04-11 08:51:44 -0700

    none

--- queries.sql
+++ queries.sql
@@ -3,5 +3,21 @@ use bamazon;
 select sum(price) from products
 where department_name='Electronics';
 
-select * from departments;
+select * from products;
 
+
+
+---------------------
+-- add dept id to products table
+UPDATE products p SET department_id = 
+(SELECT department_id FROM departments WHERE department_name = p.department_name);
+
+SELECT * FROM products;
+
+------------
+
+update departments
+set over_head_costs = 6500.99
+where department_name = 'Grocery';
+
+select * from departments;
